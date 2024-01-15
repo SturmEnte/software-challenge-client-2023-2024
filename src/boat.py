412d8e6664c563bf2b7cc48c31812c88735982ef
Jonas <sturmente@proton.me>
2024-01-15 23:30:04 +0100

    Removed old code

--- src/boat.py
+++
@@ -1,8 +0,0 @@
-class Boat:
-    passengers = 0
-    rotation = 0        # right 0, bottom-right 1, bottom-left 2, left 3, top-left 4, top-right 5
-    coal = 6
-    speed = 1
-
-    def __init_(self):
-        pass
